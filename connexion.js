7f595e77158c9f7219946c3e9b2e21d0e06bd3cf
unknown <ulrichsetin1@gmail.com>
2023-06-28 04:12:10 +0000

    test

--- connexion.js
+++ connexion.js
@@ -2,10 +2,10 @@ const mysql=require('mysql')
 const myconnect=require("express-myconnection")
 const config={
     host:"localhost",
-    user:"root",
-    password:"",
+    user:"id19980366_jaums",
+    password:"Jaums2002@",
     port:3306,
-    database:"notes"
+    database:"id19980366_flutter"
 }
 
 module.exports={
